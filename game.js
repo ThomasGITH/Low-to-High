9f50fefebf1bf601b889b4f96d72d4f37b8f98e5
DGS <games_bg@dutchgamestudio.nl>
2020-04-17 17:33:10 +0200

    random numbers in random boxes

--- game.js
+++ game.js
@@ -7,29 +7,31 @@
 
 
 let numbers = [];
+let activeSquares = [];
 
-function AssingNewNumbers(){
-    for(let i = 0; i < 5; i++){
-        let isNew = false;
-        let newNum;
-        while(isNew == false){
-           isNew = true;
-           newNum = (getRandomInt(1, 100));
-           numbers.forEach(element => {
-               if(element == newNum){
-                   isNew = false;
-               }
-           });
-        }
-        numbers[i] = newNum;
-    }
+function CreateNewNumbers(){
+    numbers = RandomNumbers(5, 100);
     console.log(numbers);
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function AssignNewNumbers(){
+    activeSquares = RandomNumbers(5, 9);
+    activeSquares.forEach(nmbr => {
+        squareID = "box" + nmbr;
+        let square = document.getElementById(squareID);
+        square.style.display = "block";
+    });
 }
 
-AssingNewNumbers();
+function RandomNumbers(length, maxValue){
+    var arr = [];
+    while(arr.length < length){
+      var r = Math.floor(Math.random() * maxValue) + 1;
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+    return arr;
+}
+
+
+CreateNewNumbers();
+AssignNewNumbers();
